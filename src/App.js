d7cea2c1a137a02910c8c445fcc6ced615625a5b
Jon Hill <jondhill1984@gmail.com>
2020-05-31 14:21:38 +0300

    Add Basic API and UI to app.js only

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import DisplayId from './scripts.js';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [data, setData] = useState([]);
+  
+  function handleChange(e) {
+    let searchValueHolder = e.target.value;
+    setSearchTerm(searchValueHolder);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const endpoint = 
+    `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyC5UvLD7agMnKHaJiK-dqPRL9T9Ton9jXM`;
+
+    fetch(endpoint)
+    .then(blob => blob.json())
+    .then(stuff => setData(stuff.items));
+
+    console.log(endpoint);
+  }
+
+  let displayBooks = data.map(book => (
+    <div>
+        <div>{book.volumeInfo.title}</div>
+        <div>{book.volumeInfo.authors}</div>
+    </div>
+  ));
+
+
   return (
   <div className="container">
     <div className="main title">Title</div>
-    <div className="main input">search area input</div>
-    <DisplayId />
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        placeholder="Search" 
+        className="main input"
+        onChange={handleChange}>
+      </input>
+      <input type="submit"></input>
+    </form>
+    <div>{displayBooks}</div>
   </div> 
   );
 }
