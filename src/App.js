e3cac6a65e52f44fdeaa4f6b531559942a6fd8f0
Jon Hill <jondhill1984@gmail.com>
2020-05-31 15:24:35 +0300

    Add basic styling and layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [data, setData] = useState([]);
-  
+  console.log(data);
+
   function handleChange(e) {
     let searchValueHolder = e.target.value;
     setSearchTerm(searchValueHolder);
@@ -19,31 +20,36 @@ function App() {
     fetch(endpoint)
     .then(blob => blob.json())
     .then(stuff => setData(stuff.items));
-
-    console.log(endpoint);
   }
 
   let displayBooks = data.map(book => (
-    <div>
-        <div>{book.volumeInfo.title}</div>
-        <div>{book.volumeInfo.authors}</div>
+    <div className="book">
+      <a href={book.volumeInfo.previewLink}>
+        <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="book cover"/>
+      </a>
+      <div className="bookTitle">{book.volumeInfo.title}</div>
+      <div className="bookAuthor">{book.volumeInfo.authors}</div>
     </div>
   ));
-
-
+  
   return (
   <div className="container">
-    <div className="main title">Title</div>
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        placeholder="Search" 
-        className="main input"
-        onChange={handleChange}>
-      </input>
-      <input type="submit"></input>
-    </form>
-    <div>{displayBooks}</div>
+    <div className="main title">Find A Book</div>
+    <div className="formContainer">
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Search" 
+          className="main input"
+          onChange={handleChange}>
+        </input>
+        <input className="submit" type="submit" id="submit"></input>
+        <label className="SubmitButtonLabel" htmlFor="submit"><span>Search</span></label>
+      </form>
+    </div>
+    <div className="displayArea">
+      {displayBooks}
+    </div>
   </div> 
   );
 }
